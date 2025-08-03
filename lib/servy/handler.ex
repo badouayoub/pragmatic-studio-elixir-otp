ed61ad894d24ebe7c0a884b7009202eccc365c90
badouayoub <badou.ayoub@gmail.com>
2025-08-03 15:54:19 +0900

    7. Advanced Pattern Matching

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -16,7 +16,7 @@ defmodule Servy.Handler do
       |> List.first()
       |> String.split(" ")
 
-    %{method: method, path: path, resp_body: ""}
+    %{method: method, path: path, resp_body: "", status: nil}
   end
 
   def route(conv) do
@@ -27,25 +27,45 @@ defmodule Servy.Handler do
   # Pattern match on map route(%{method: "GET", path: "/wildthings"} = conv)
   # Will be done in module 8
   def route(conv, "GET", "/wildthings") do
-    %{conv | resp_body: "Bears, Lions, Tigers"}
+    %{conv | status: 200, resp_body: "Bears, Lions, Tigers"}
   end
 
   def route(conv, "GET", "/bears") do
-    %{conv | resp_body: "Rachid, Hmida, Bibich"}
+    %{conv | status: 200, resp_body: "Rachid, Hmida, Bibich"}
   end
 
-  def route(conv, "GET", "/bigfoot") do
-    %{conv | resp_body: "Kbirfoot"}
+  def route(conv, "GET", "/bears/" <> id) do
+    %{conv | status: 200, resp_body: "Bear #{id}"}
+  end
+
+  def route(conv, "DELETE", "/bears/" <> _id) do
+    %{conv | status: 403, resp_body: "Deleting a bear is forbidden!"}
+  end
+
+  # Catch all route ie Error
+  def route(conv, _method, path) do
+    %{conv | status: 404, resp_body: "No #{path} here!"}
   end
 
   def format_response(conv) do
     """
-    HTTP/1.1 200 OK
+    HTTP/1.1 #{conv.status} #{status_code(conv.status)}
     Content-Type: text/html
     Content-Length: #{byte_size(conv.resp_body)}
 
     #{conv.resp_body}
     """
+  end
+
+  defp status_code(code) do
+    %{
+      200 => "OK",
+      201 => "Created",
+      401 => "Unauthorized",
+      403 => "Forbidden",
+      404 => "Not Found",
+      500 => "Internal Server Error"
+    }[code]
   end
 end
 
@@ -58,7 +78,6 @@ Accept: */*
 """
 
 response = Servy.Handler.handle(request)
-
 IO.puts(response)
 
 request = """
@@ -70,7 +89,6 @@ Accept: */*
 """
 
 response = Servy.Handler.handle(request)
-
 IO.puts(response)
 
 request = """
@@ -82,5 +100,26 @@ Accept: */*
 """
 
 response = Servy.Handler.handle(request)
+IO.puts(response)
 
+request = """
+GET /bears/1 HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+IO.puts(response)
+
+request = """
+DELETE /bears/1 HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
 IO.puts(response)
